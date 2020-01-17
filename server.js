7663dd2fcc13ab240fade065e2ecf7de885e2f9c
Theodore Pantazopoulos <teopanta1986@gmail.com>
2020-01-17 14:38:25 +0200

    url

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const mongoose = require("mongoose");
 
 //connect to mongodb
 mongoose
-  .connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@dswhatever`, { useNewUrlParser: true })
+  .connect(`mongodb://heroku_skf9lr6x:nsqnc9a1liulkjoteg9tg2e93t@ds263848.mlab.com:63848/heroku_skf9lr6x`)
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB..."));
-  
+
 app.get("/", (req, res) => res.json({ status: 200, message: "hello" }));
 
 app.use('/api', routes.api);
