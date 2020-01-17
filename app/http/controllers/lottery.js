e6e9448297120009d72768bb499c3f792902becb
Nikiforos <nikifmak@gmail.com>
2020-01-17 17:05:22 +0200

    added new method for calculatiln of winners

--- app/http/controllers/lottery.js
+++ app/http/controllers/lottery.js
@@ -1,55 +1,55 @@
-
-const logger = require('../../helpers/logger');
-const Config = require('../../../config');
-const User = require('../../../models/user');
-const Lottery = require('../../../models/lottery');
-const { IncomingWebhook } = require('@slack/webhook');
+const logger = require("../../helpers/logger");
+const Config = require("../../../config");
+const User = require("../../../models/user");
+const Lottery = require("../../../models/lottery");
+const { IncomingWebhook } = require("@slack/webhook");
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
 
 exports.end = async (request, response) => {
+  console.log("eeedd");
   let winners;
-  const lottery = await Lottery
-      .where({ active: true })
-      .findOne();
-  if (lottery) { 
-    
+
+  const lottery = await Lottery.where({ active: true }).findOne();
+
+  console.log(lottery);
+
+  if (lottery) {
+    startRandomSelection(lottery);
+
     winners = lottery.winners;
+
     if (winners.length === 0) {
-      winners = ['Leo', 'Nik', 'Teo', 'Dimitris'];
+      winners = ["Leo", "Nik", "Teo", "Dimitris"];
     }
-    console.log(winners)
-    webhook.send('Winners Are:' + winners.join(','), (err, res) => {
+    console.log(winners);
+    webhook.send("Winners Are:" + winners.join(","), (err, res) => {
       if (err) {
-        console.log('Error:', err);
-        response.customSuccess('ok');
+        console.log("Error:", err);
+        response.customSuccess("ok");
       } else {
         // console.log('Message sent: ', res);
-        response.customSuccess('ok');
+        response.customSuccess("ok");
       }
     });
   } else {
-    return response.customError('No active lottery');
+    return response.customError("No active lottery");
   }
 
-  webhook.send('Event winners are: !', (err, res) => {
+  webhook.send("Event winners are: !", (err, res) => {
     if (err) {
-      console.log('Error:', err);
-      response.customSuccess('ok');
+      console.log("Error:", err);
+      response.customSuccess("ok");
     } else {
       response.customSuccess();
     }
   });
-
 };
 
 exports.create = async (request, response) => {
-
-  const oldLottery = await Lottery
-      .where({ active: true })
-      .findOne();
-  if (oldLottery) { 
-    return response.customError('There is already an active lottery');
+  const oldLottery = await Lottery.where({ active: true }).findOne();
+  if (oldLottery) {
+    return response.customError("There is already an active lottery");
   } else {
     const data = request.body;
     data.active = true;
@@ -60,20 +60,33 @@ exports.create = async (request, response) => {
 
     return response.customSuccess();
   }
-
-}
+};
 
 exports.cancel = async (request, response) => {
-
-  const oldLottery = await Lottery
-      .where({ active: true })
-      .findOne();
-  if (oldLottery) { 
+  const oldLottery = await Lottery.where({ active: true }).findOne();
+  if (oldLottery) {
     oldLottery.active = false;
     await oldLottery.save();
-    return response.customSuccess('Lottery Cancelled');
+    return response.customSuccess("Lottery Cancelled");
   } else {
-    return response.customError('No active lottery found');
+    return response.customError("No active lottery found");
   }
+};
 
+async function startRandomSelection(lottery) {
+  let participantsIds = lottery.participants
+    .filter((el) => el !== null)
+    .filter(function(elem, index, self) {
+      return index === self.indexOf(elem);
+    });
+
+  console.log("participantsIds ", participantsIds);
+
+  let participants = await User.find({
+    slack_id: { $in: participantsIds }
+  });
+
+  console.log(participants);
+
+  return participants;
 }
