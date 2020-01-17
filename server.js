a2aaa3936fa85ff6580ed07e979645e917e2080c
Theodore Pantazopoulos <teopanta1986@gmail.com>
2020-01-17 12:09:13 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+
+//connect to mongodb
+mongoose
+  .connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@dswhatever`, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
 
 app.get("/", (req, res) => res.json({ status: 200, message: "hello" }));
 
